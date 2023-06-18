679e01f81a6a7f6b1e0a6ac8c8b58761cca09578
Alessandro <ale.contestabile@gmail.com>
2023-06-18 20:02:34 +0200

    Esercizio final Javascript modulo 2

---
+++ finaleJavascript.js
@@ -0,0 +1,190 @@
+/*
+PARTE 1: 
+Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
+Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
+Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
+L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
+Prendi queste tre inserzioni ad esempio:
+
+      job1:  location: "NY, US",     title: "java dev"
+      job2:  location: "Genoa, IT"   title: "web dev"
+      job3:  location: "US"      title: "dev"
+
+Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
+in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
+
+REQUISITI:
+- il tuo algoritmo deve tornare i risultati nella seguente forma:
+{
+  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
+  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
+}
+
+- da ogni inserzione trovata, elimina i campi "description", "requirements", "benefits" e "company_profile" per semplificare il risultato
+
+- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
+
+
+PARTE 2: 
+Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
+
+Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
+
+Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
+    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+    SUGGERIMENTO: ti servira’ un ciclo for!
+
+*/
+
+// NON MODIFICARE QUESTO ARRAY!
+const jobs = [
+  { title: "Marketing Intern", location: "US, NY, New York" },
+  {
+    title: "Customer Service - Cloud Video Production",
+    location: "NZ, Auckland",
+  },
+  {
+    title: "Commissioning Machinery Assistant (CMA)",
+    location: "US, IA, Wever",
+  },
+  {
+    title: "Account Executive - Washington DC",
+    location: "US, DC, Washington",
+  },
+  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+  { title: "Accounting Clerk", location: "US, MD," },
+  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+  {
+    title: "Lead Guest Service Specialist",
+    location: "US, CA, San Francisco",
+  },
+  { title: "HP BSM SME", location: "US, FL, Pensacola" },
+  {
+    title: "Customer Service Associate - Part Time",
+    location: "US, AZ, Phoenix",
+  },
+  {
+    title: "ASP.net Developer Job opportunity at United States,New Jersey",
+    location: "US, NJ, Jersey City",
+  },
+  {
+    title: "Talent Sourcer (6 months fixed-term contract)",
+    location: "GB, LND, London",
+  },
+  {
+    title: "Applications Developer, Digital",
+    location: "US, CT, Stamford",
+  },
+  { title: "Installers", location: "US, FL, Orlando" },
+  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+  {
+    title: "VP of Sales - Vault Dragon",
+    location: "SG, 01, Singapore",
+  },
+  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+  {
+    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+    location: "GB, SOS, Southend-on-Sea",
+  },
+  { title: "Visual Designer", location: "US, NY, New York" },
+  {
+    title: "Process Controls Engineer - DCS PLC MS Office - PA",
+    location: "US, PA, USA Northeast",
+  },
+  { title: "Marketing Assistant", location: "US, TX, Austin" },
+  { title: "Front End Developer", location: "NZ, N, Auckland" },
+  { title: "Engagement Manager", location: "AE," },
+  {
+    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+    location: "US, CA, Carlsbad",
+  },
+  { title: "Customer Service", location: "GB, LND, London" },
+  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+  { title: "Marketing Exec", location: "SG," },
+  {
+    title: "HAAD/DHA Licensed Doctors Opening in UAE",
+    location: "AE, AZ, Abudhabi",
+  },
+  {
+    title: "Talent Management Process Manager",
+    location: "US, MO, St. Louis",
+  },
+  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+  {
+    title: "Customer Service Technical Specialist",
+    location: "US, MA, Waltham",
+  },
+  { title: "Software Applications Specialist", location: "US, KS," },
+  { title: "Craftsman Associate", location: "US, WA, Everett" },
+  { title: "Completion Engineer", location: "US, CA, San Ramon" },
+  { title: "I Want To Work At Karmarama", location: "GB, LND," },
+  {
+    title: "English Teacher Abroad",
+    location: "US, NY, Saint Bonaventure",
+  },
+];
+
+// SVOLGIMENTO con commenti***
+
+//ciclo l'array dei JOBS con i dati che mi passa la funzione "validare()", a questo punto  rendo il contenuto delle chiavi dell'array jobs
+// tutto minuscolo e ne verifico la presenza con .includes() ritorno poi un'oggetto
+function searchJobs(posLav, posGeo) {
+  let findedJobs = [];
+  let numberJobs = 0;
+  for (const inserzioni of jobs) {
+    if (
+      inserzioni.title.toLowerCase().includes(posLav) == true &&
+      inserzioni.location.toLowerCase().includes(posGeo) == true
+    ) {
+      findedJobs.push(inserzioni);
+    }
+  }
+  numberJobs = findedJobs.length;
+
+  return {
+    result: findedJobs,
+    count: numberJobs,
+  };
+}
+
+// quando clicco il pulsante di ricerca, prendo i dati da i due campi in input del form e li passo come parametri alla funzione di ricerca "searcJobs"
+function validare() {
+  let x = document.getElementById("geo").value.toLowerCase();
+  let y = document.getElementById("job").value.toLowerCase();
+  let resReturn = searchJobs(y, x);
+  console.log(resReturn);
+  createHtml(resReturn);
+}
+
+//creo una funzione per creare gli elementi html in formato tabellare,
+//ho cercato di utilizzare entrambi gli elementi, sia appenChild che innerHTML
+function createHtml(resReturn) {
+  document.querySelector(
+    "#results p"
+  ).innerHTML = `Abbiamo trovato ${resReturn.count} risultati che soddisfano la tua ricerca`;
+
+  if (resReturn.count != 0) {
+    document.querySelector(".fa-solid").style.display = "none";
+    document.getElementById("mostra_risultati").innerHTML =
+      "<tr><th>FIGURA RICERCATA</th><th>LUOGO DI LAVORO</th></tr>";
+
+    for (const risultati of resReturn.result) {
+      let tabella = document.getElementById("mostra_risultati");
+      let rigo = document.createElement("tr");
+      rigo.style.animation = `myAnimation 1s linear`;
+
+      let colonnaJob = document.createElement("td");
+      colonnaJob.innerHTML = `${risultati.title}`;
+      let colonnaLocation = document.createElement("td");
+      colonnaLocation.innerHTML = `${risultati.location}`;
+
+      rigo.appendChild(colonnaJob);
+      rigo.appendChild(colonnaLocation);
+      tabella.appendChild(rigo);
+    }
+  } else {
+    document.getElementById("mostra_risultati").innerHTML = "";
+    document.querySelector(".fa-solid").style.display = "block";
+  }
+}
